c6be5f273ae187cd6ea60673650d17ff38c1d265
rammfall <15bogdan17@gmail.com>
2024-11-14 19:46:16 +0200

    Update test config

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig(() => ({
   root: join(process.cwd(), '/src'),
+  test: {
+    globals: true,
+    window: true,
+    environment: 'jsdom',
+  },
 }))
